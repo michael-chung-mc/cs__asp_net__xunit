08487a1053d28ad5b4a0b37459332143ddc04105
michael-chung-mc <chung.michael.mc@gmail.com>
2023-11-09 19:54:09 -0500

    init mvc tut

---
+++ lib/MvCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller {
+    public string Index() {
+        return "default";
+    }
+    public string Welcome() {
+        return "welcome";
+    }
+    public string Params(string name, int numTimes = 1) {
+        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    }
+    public string ParamID(string name, int ID = 1) {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {ID}");
+    }
+}
---
+++ lib/MvCMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MvCMovie.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
